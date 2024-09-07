9738ae14ecdb1742a907b9568ec2b7a8c95bdfec
koki-table <koki.s0327@icloud.com>
2024-09-07 17:20:21 +0900

    feat: サンプルのUI追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+
+  const handleNavigate = () => {
+    router.push('/sample');
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <Button>Click me</Button>
+        <Button onClick={handleNavigate}>Click me</Button>
       </div>
     </main>
   );
---
+++ src/app/sample/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function sample() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <Button>sample</Button>
+      </div>
+    </main>
+  );
+}
